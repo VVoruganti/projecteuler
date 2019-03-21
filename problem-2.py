cf6c66cedb91ca9c506d234e4300a1d7ce1368ec
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2019-03-20 23:05:24 -0400

    Finished problems 2 and 3

--- problem-2.py
+++ problem-2.py
@@ -1,4 +1,13 @@
 def fib( prev1, prev2, maxVal, curSum):
     sum = curSum
+    if (prev2 % 2 == 0):
+        sum += prev2
     if (prev1 + prev2 < maxVal):
+        return fib(prev2, prev1 + prev2, maxVal, sum)
+    else:
+        return sum
     
+x = fib(1,2,4000000,0)
+print(x)
+
+
