e8fe15b8f8037ca0ed1006eca8a0b2a22f6d0b79
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2019-12-01 14:18:42 -0800

    problem 6 and start of 7

---
+++ problem-6.py
@@ -0,0 +1,12 @@
+
+count = 0
+count2 = 0 
+for i in range(1, 101):
+   count += (i * i) 
+   count2 += i
+   print(i)
+
+print(count)
+print(count2)
+t = (count2 * count2) - count
+print(t)
---
+++ problem-7.py
@@ -0,0 +1,11 @@
+def prime(x):
+    limit = round(x/2)
+    for i in range(2, limit)
+    return True
+
+primes = [ ] 
+
+counter = 2 
+while (len(primes) < 10001 ):
+    if(prime(counter)):
+        primes.append(counter)
